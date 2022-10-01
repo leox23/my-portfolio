2ef40130c828869e7a0bcdfe581e2a2b38d155b8
leo23 <leox-23@hotmail.com>
2022-10-01 17:44:35 -0500

    about section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import About from '../components/About';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 
@@ -19,6 +20,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* About */}
+      <section id="about" className="snap-center">
+        <About/>
+      </section>
 
       {/* Expirience */}
 
