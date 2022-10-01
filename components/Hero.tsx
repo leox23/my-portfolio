a5a571c7bcbec9e74e779f9ccf99e2c84690b80b
leo23 <leox-23@hotmail.com>
2022-10-01 16:56:16 -0500

    main structure and details of hero s

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,61 +1,64 @@
-import { useState } from "react";
-import { Typewriter } from "./Typewriter";
 import { Cursor } from "./Cursor";
 import { useTypewriter } from "../Hooks/useTypewriter";
-/*import "./styles.module.css";*/
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
 
-/**
- * Old demo
- * https://codesandbox.io/s/react-typewriting-effect-8ulgs
- *
- * Github
- * https://github.com/awran5/react-simple-typewriter/
- */
+export default function Hero() {
+  const TypewriterHook = () => {
+    const [text, count] = useTypewriter({
+      words: [
+        "Hi, Mi name is Leonel Mira",
+        "Guy-who-loves-workout.tsx",
+        "<ButLovesToCodeMore/>",
+        "And scripting with AutoHotKey!",
+      ],
+      loop: 0,
+      delaySpeed: 2000,
+      // typeSpeed: 20,
+      //onLoopDone: () => console.log("done from typewriter hook")
+    });
 
-const TypewriterHook = () => {
-  const [text, count] = useTypewriter({
-    words: ["Hello", "World", "This", "is", "Typewriter", "Hook"],
-    loop: 2,
-    // typeSpeed: 20,
-    onLoopDone: () => console.log("done from typewriter hook")
-  });
+    return (
+      <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+        <BackgroundCircles />
+        {/*todo pending change to <Image/> component*/}
+        <img
+          className="relative rounded-full h-32 w-32 mx-auto object-cover"
+          src="https://avatars.githubusercontent.com/u/21028236?v=4"
+          alt="github profile user image"
+        />
+
+        <div className="z-20">
+          <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+            FrontEnd Engineer
+          </h2>
+
+          <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+            <span>{text}</span>
+            <Cursor cursorColor="#F7AB0A" />
+          </h1>
+
+          <div className="pt-5">
+            <Link href="#about">
+              <button className="heroButtons">About</button>
+            </Link>
+            <Link href="#experience">
+              <button className="heroButtons">Experience</button>
+            </Link>
+            <Link href="#skills">
+              <button className="heroButtons">Skills</button>
+            </Link>
+            <Link href="#projects">
+              <button className="heroButtons">Projects</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div>
-      <span>{text}</span>
-      <Cursor />
-      <hr />
-      <div>count: {count}</div>
-    </div>
-  );
-};
-
-export default function App() {
-  const [count, setCount] = useState(0);
-  const handleDone = () => {
-    console.log("done from typewriter component");
-  };
-  const handleType = (counter: number) => {
-    setCount(counter);
-    console.log("this function run every type on Component");
-  };
-
-  return (
-    <div style={{ padding: 20 }}>
-      <p>The default component</p>
-
-      <Typewriter
-        words={["Hello", "World", "This", "is", "Typewriter", "Component"]}
-        loop={1}
-        // typeSpeed={40}
-        cursor
-        onLoopDone={handleDone}
-        onType={handleType}
-      />
-      <hr />
-      <div>count: {count}</div>
-      <p>A simple custom typewriter built with the hook!</p>
-
       <TypewriterHook />
     </div>
   );
