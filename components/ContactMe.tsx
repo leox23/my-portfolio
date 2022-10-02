4b798639a1998be446025e6e54d469dba766be85
leo23 <leox-23@hotmail.com>
2022-10-02 11:46:53 -0500

    testing react hook form

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { PhoneIcon, EnvelopeIcon,MapPinIcon } from "@heroicons/react/24/solid";
+import { PhoneIcon, EnvelopeIcon, MapPinIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+    name: string;
+    email: string;
+    subject: string;
+    message: string;
+}
 
 type Props = {};
 
 function ContactMe({}: Props) {
+    const { register, handleSubmit } = useForm<Inputs>()
+    const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -32,15 +42,15 @@ function ContactMe({}: Props) {
             <p className="text-2xl">Medellin colombia</p>
           </div>
         </div>
-      <form className="flex flex-col space-y-2 w-fit mx-auto">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
         <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput w-[100%]" type="email" />
+            <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
+            <input {...register('email')} placeholder="Email" className="contactInput w-[100%]" type="email" />
         </div>
 
-        <input placeholder="Subject"  className="contactInput" type="text" />
+        <input {...register('subject')} placeholder="Subject"  className="contactInput" type="text" />
       
-        <textarea placeholder="Message" className="contactInput"></textarea>
+        <textarea {...register('message')} placeholder="Message" className="contactInput"></textarea>
 
         <button 
         type="submit"
