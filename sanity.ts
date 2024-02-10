975582127c3348fa2add30c3657fcc66f6d73987
leohn23 <leo@fipfit.com>
2024-02-10 14:52:30 -0500

    sanity config

--- sanity.ts
+++ sanity.ts
@@ -2,9 +2,10 @@ import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
 export const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    dataset: process.env.SANITY_API_DATASET || process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: process.env.SANITY_API_PROJECT_ID || process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     apiVersion: "2021-03-25",
+    token: process.env.SANITY_API_WRITE_TOKEN,
     useCdn: process.env.NODE_ENV !== "production"
 };
 
